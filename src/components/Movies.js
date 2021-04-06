2b7d655f81232c89769b3667e32131b40324de59
kinnari <kinnari1022@yahoo.com>
2021-04-06 19:08:46 -0400

    commit tree 68f27228efc9cecadd304959a10d401feb50d96f

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,11 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      <div>
+        <p>Movie Title: </p>
+        <ul>Genres: </ul>
+      </div>
     </div>
   );
 };
